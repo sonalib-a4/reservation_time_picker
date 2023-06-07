f31c38c08557fb467566a8cfab9b4c1b33b5cfd4
samruddhi.gorghate <samruddhi.gorghate@afourtech.com>
2023-06-07 15:08:12 +0530

    side bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Login from './login';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-
+import "./App.css";
+import Login from "./login";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Workspace from "./Pages/Workspace";
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/workspace" element={<Workspace />} />
         </Routes>
       </Router>
     </div>
---
+++ src/Pages/Workspace.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../Components/Sidebar";
+import { Typography } from "@mui/material";
+
+const Workspace = () => {
+  return (
+    <React.Fragment>
+      <Typography>Workspace</Typography>
+      <Sidebar></Sidebar>
+    </React.Fragment>
+  );
+};
+
+export default Workspace;
